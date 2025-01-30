21b0349c3a7e43e0ed4339c31a8efdfb29b46839
Tracy Tucker <tracytuckeremail@gmail.com>
2025-01-30 17:50:29 -0600

    Change header copy

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,7 +7,7 @@ const Hero = () => {
         <div className="hero-banner">
           <h1>backroads app</h1>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae
+            Lorem ipsumish dolor sit amet consectetur adipisicing elit. Vitae
             explicabo debitis est autem dicta.
           </p>
           <a href="#tours" className="btn hero-btn">
